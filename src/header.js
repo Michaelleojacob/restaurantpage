368a5b13867b5fa3f32c9b5d4ec3df97ceb11771
Michael <michaelleojacob@gmail.com>
2021-07-18 11:34:43 -0700

    header is looking pretty decent. Happy with how it is turning out.

--- src/header.js
+++ src/header.js
@@ -6,15 +6,50 @@ export default function header() {
 		const info = {
 			init: function () {
 				this.cacheDom();
-				this.testing();
+				this.headwrapper();
+				this.restaurantName();
+				this.buttonwrap();
+				// this.buttonOne();
 			},
 			cacheDom: function () {
 				this.content = document.querySelector('#content');
 			},
-			testing: function () {
-				this.test = document.createElement('div');
-				this.test.textContent = 'i would like this text to be green';
-				this.content.appendChild(this.test);
+			headwrapper: function () {
+				this.hwrapper = document.createElement('div');
+				this.hwrapper.classList.add('hwrapper');
+				this.content.appendChild(this.hwrapper);
+			},
+			restaurantName: function () {
+				this.restname = document.createElement('div');
+				this.restname.textContent = 'Butter Toast';
+				this.restname.classList.add('btoast');
+				this.hwrapper.appendChild(this.restname);
+			},
+			buttonwrap: function () {
+				this.bwrap = document.createElement('div');
+				this.bwrap.classList.add('bwrap');
+				this.hwrapper.appendChild(this.bwrap);
+				this.buttonOne();
+				this.buttonTwo();
+				this.buttonThree();
+			},
+			buttonOne: function () {
+				this.bone = document.createElement('button');
+				this.bone.textContent = 'about';
+				this.bone.classList.add('bone', 'btn');
+				this.bwrap.appendChild(this.bone);
+			},
+			buttonTwo: function () {
+				this.btwo = document.createElement('button');
+				this.btwo.textContent = 'menu';
+				this.btwo.classList.add('btwo', 'btn');
+				this.bwrap.appendChild(this.btwo);
+			},
+			buttonThree: function () {
+				this.bthree = document.createElement('button');
+				this.bthree.textContent = 'contact';
+				this.bthree.classList.add('bthree', 'btn');
+				this.bwrap.appendChild(this.bthree);
 			},
 		};
 		info.init();
